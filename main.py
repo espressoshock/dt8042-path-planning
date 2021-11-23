156320ec47b89c48d289a66102399c2ca22b0227
Vincenzo Buono <vincebshock@gmail.com>
2021-11-23 09:31:36 +0100

    ✨Main: basic simulation workflow added

--- main.py
+++ main.py
@@ -3,11 +3,36 @@
 ##############################
 
 # =Imports
+from matplotlib import pyplot as plt
+import numpy as np
+from search import Search
+from search_bfs import SearchBFS
 from simulation import Simulation
+from graph import Graph, SimpleGraph, SquareGridGraph
 
 
 def main():
-    Simulation.test()
+    '''
+    # TESTING
+
+    map = Simulation.generate_2dmap([60, 60])
+    bfs = SearchBFS()
+    start = Simulation.extract_start_pos(map)
+    goal = Simulation.extract_goal_pos(map)
+    #print(map)
+    sq = SquareGridGraph(len(map), len(map), map)
+    bfsres = bfs.search(sq, start, goal)
+    #print(sq.walls)
+    #Simulation.display_plot(map)
+    #print('goal: ', goal)
+    #Simulation.plot_map(map, bfsres, 'Result')
+    #Simulation.test()
+    '''
+    search = SearchBFS()
+
+    sim = Simulation(search)
+    sim.generate_random_map()
+    sim.start()
 
 
 if __name__ == '__main__':
