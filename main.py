7e6e110470d3fa59b1e01121a1fd0e07b0537892
Vincenzo Buono <vincebshock@gmail.com>
2021-11-23 05:56:36 +0100

    ✨ Main class added

---
+++ main.py
@@ -0,0 +1,14 @@
+##############################
+### Task 1 : Main entry Point
+##############################
+
+# =Imports
+from simulation import Simulation
+
+
+def main():
+    Simulation.test()
+
+
+if __name__ == '__main__':
+    main()
